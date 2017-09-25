969693e3cbd25b18ceecebc418adc2f275ced915
Penguin-007 <mr.laznevoy@mail.ru>
2017-09-26 00:37:21 +0300

    0.0.02
    add updater

--- functions.php
+++ functions.php
@@ -1,1 +1,8 @@
 <?php
+
+require 'update_checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+  'https://github.com/penguin-007/wps_framework',
+  __FILE__,
+  'wps_framework'
+);
